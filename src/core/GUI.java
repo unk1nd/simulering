78fd95d85ec185e8f06b91bd965f5fa384bfcf16
Mikael Bendiksen <duuxman@gmail.com>
2013-04-11 12:32:56 +0200

    gui påbynt

--- src/core/GUI.java
+++ src/core/GUI.java
@@ -8,12 +8,20 @@
 
 package core;
 
-public class GUI 
+import javax.swing.*;
+
+public class GUI extends JFrame
 {
 	public static void main(String[] args) 
 	{
-		
-
+		JFrame frame = new JFrame ("Simulering");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 500);
+ 	   	frame.setResizable(false);
+ 	   	frame.setLocationRelativeTo(null);
+ 	   	frame.setVisible(true);
+ 	   	
+ 	   	
 	}
 
 }
