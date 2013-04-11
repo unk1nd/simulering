99754de1e770dab7e781a975014d030c19df6a1b
Mikael Bendiksen <duuxman@gmail.com>
2013-04-11 12:14:53 +0200

    upload

---
+++ src/core/GUI.java
@@ -0,0 +1,19 @@
+
+	/**
+	 * 		Systemutvikling 2013
+	 * 		Gruppe 2
+	 * 		simuleringscore for str¿m
+	 */
+
+
+package core;
+
+public class GUI 
+{
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
---
+++ src/core/XML.java
@@ -0,0 +1,146 @@
+package core;
+
+/**
+ * 		Systemutvikling 2013
+ * 		Gruppe 2
+ * 		simuleringscore for str¿m
+ * 		XML reader/writer class
+ */
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+
+import org.xml.sax.*;
+import org.w3c.dom.*;
+
+
+public class XML 
+{
+	private String role1 = null;
+	private String role2 = null;
+	private String role3 = null;
+	private String role4 = null;
+	private ArrayList<String> rolev;
+	
+	public boolean readXML(String xml) {
+        rolev = new ArrayList<String>();
+        Document dom;
+        // Make an  instance of the DocumentBuilderFactory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            // use the factory to take an instance of the document builder
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            // parse using the builder to get the DOM mapping of the    
+            // XML file
+            dom = db.parse(xml);
+
+            Element doc = dom.getDocumentElement();
+
+            role1 = getTextValue(role1, doc, "role1");
+            if (role1 != null) {
+                if (!role1.isEmpty())
+                    rolev.add(role1);
+            }
+            role2 = getTextValue(role2, doc, "role2");
+            if (role2 != null) {
+                if (!role2.isEmpty())
+                    rolev.add(role2);
+            }
+            role3 = getTextValue(role3, doc, "role3");
+            if (role3 != null) {
+                if (!role3.isEmpty())
+                    rolev.add(role3);
+            }
+            role4 = getTextValue(role4, doc, "role4");
+            if ( role4 != null) {
+                if (!role4.isEmpty())
+                    rolev.add(role4);
+            }
+            return true;
+
+        } catch (ParserConfigurationException pce) {
+            System.out.println(pce.getMessage());
+        } catch (SAXException se) {
+            System.out.println(se.getMessage());
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+
+        return false;
+    }
+	
+	private String getTextValue(String def, Element doc, String tag) {
+	    String value = def;
+	    NodeList nl;
+	    nl = doc.getElementsByTagName(tag);
+	    if (nl.getLength() > 0 && nl.item(0).hasChildNodes()) {
+	        value = nl.item(0).getFirstChild().getNodeValue();
+	    }
+	    return value;
+	}
+	
+	public void saveToXML(String xml) 
+	{
+	    Document dom;
+	    Element e = null;
+
+	    // instance of a DocumentBuilderFactory
+	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	    try {
+	        // use factory to get an instance of document builder
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+	        // create instance of DOM
+	        dom = db.newDocument();
+
+	        // create the root element
+	        Element rootEle = dom.createElement("roles");
+
+	        // create data elements and place them under root
+	        e = dom.createElement("role1");
+	        e.appendChild(dom.createTextNode(role1));
+	        rootEle.appendChild(e);
+
+	        e = dom.createElement("role2");
+	        e.appendChild(dom.createTextNode(role2));
+	        rootEle.appendChild(e);
+
+	        e = dom.createElement("role3");
+	        e.appendChild(dom.createTextNode(role3));
+	        rootEle.appendChild(e);
+
+	        e = dom.createElement("role4");
+	        e.appendChild(dom.createTextNode(role4));
+	        rootEle.appendChild(e);
+
+	        dom.appendChild(rootEle);
+
+	        try {
+	            Transformer tr = TransformerFactory.newInstance().newTransformer();
+	            tr.setOutputProperty(OutputKeys.INDENT, "yes");
+	            tr.setOutputProperty(OutputKeys.METHOD, "xml");
+	            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	            tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+	            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+	            // send DOM to file
+	            tr.transform(new DOMSource(dom), 
+	                                 new StreamResult(new FileOutputStream(xml)));
+
+	        } catch (TransformerException te) {
+	            System.out.println(te.getMessage());
+	        } catch (IOException ioe) {
+	            System.out.println(ioe.getMessage());
+	        }
+	    } catch (ParserConfigurationException pce) {
+	        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+	    }
+	}
+
+
+}
